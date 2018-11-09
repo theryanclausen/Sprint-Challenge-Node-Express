c44ff696beb4b0873aa5e5a074501c7d1a041fd5
theryanclausen <theryanclausen@gmail.com>
2018-11-09 10:35:44 -0700

    post

--- config/routeMaker.js
+++ config/routeMaker.js
@@ -1,21 +1,61 @@
-const express = require('express')
+const express = require("express");
 
+module.exports = (db, resource) => {
+  const sendError = (status, errorMessage, res) => {
+    res.status(status).json({ error: errorMessage });
+  };
+  const route = express.Router();
+  route.get("/", async (req, res) => {
+    try {
+      const items = await db.get();
+      res.status(200).json(items);
+    } catch (err) {
+      sendError(500, `Server error. ${resource}s could not be found.`, res);
+    }
+  });
 
-module.exports = (db, resource) =>{
-    const sendError = (status, errorMessage, res) => {
-        res.status(status).json({ error: errorMessage });
-      };
-    const route = express.Router()
-    route.get('/', async (req,res) =>{
-        try{
-            const items = await db.get()
-            res.json(items)
+  route.get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await db.get(id);
+      res.status(200).json(item);
+    } catch (err) {
+        sendError(404, `no ${resource} found at that id.`,res)
+    }
+  });
+  if (db.getProjectActions) {
+      route.get('/:id/actions', async (req,res) =>{
+          try {
+              const {id}= req.params;
+              const actions = await db.getProjectActions(id)
+              res.status(200).json(actions)
+          }catch (err) {
+            sendError(404, `no ${resource} found at that id.`,res)
         }
-        catch(err){
-            sendError(500, `${resource}s could not be found.`, res)
+      })
+  }
+  route.post('/', async (req, res) =>{
+      try{
+        const body = req.body
+        if (resource === 'project'){
+            if (!body.name || !body.description){
+                sendError(400, `Project must include name and description`, res)
+                return;
+            }
         }
-    })
+        if (resource === 'action'){
+            if (!body.project_id || !body.description || !body.notes){
+                sendError(400, `Action must include project_id, description, and notes.`)
+                return;
+            }
+        }
+        const newItem = await db.insert(body)
+        res.status(201).json(newItem)
+      }
+      catch(err){
+          sendError(500, `${resource} could not be added.`, res)
+      }
+  })
 
-
-    return route
-}
+  return route;
+};
